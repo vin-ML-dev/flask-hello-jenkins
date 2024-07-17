7accb23659a21f833991a1ea5a31b6a1f8877b78
vin-ML-dev <vinreena15@gmail.com>
2024-07-17 23:35:30 +0530

    update jenkins7

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
+import os
 
 
 class IrisModelTrainer:
