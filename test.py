b3d7846567b3b61ed76737cfeedcccf15f1e8b7c
vin-ML-dev <vinreena15@gmail.com>
2024-07-27 09:07:59 +0530

    add files4

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ with open('iris_model.pkl', 'rb') as f:
 # Load or create your test data
 # Here, we're using the same X_test from the previous step
 # In practice, you will have your own test data
- X_test = [[0.1,1.2,2.1,1.4]]
+X_test = [[0.1,1.2,2.1,1.4]]
 
 # Make predictions
 prediction = model.predict(X_test)
