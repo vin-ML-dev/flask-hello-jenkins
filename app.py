c65a77d5433bf0feaf92e682752e1cfe7944fc17
vin-ML-dev <vinreena15@gmail.com>
2024-07-27 09:17:45 +0530

    add files5

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify,request
 import os
 import pickle
+import numpy as np
 
 
 app = Flask(__name__)
 
+def make_predict(sample):
+   with open('iris_model.pkl', 'rb') as f:
+       model = pickle.load(f)
+       
+   prediction = model.predict(sample)
+
+   return prediction
+   
+   
 @app.route("/")
 def hello():
    return render_template('home.html')
+
+@pp.route('predict/')
+def predict():
+
+   t = request.json['test_data']
+   sample = np.array([t])
+   pred = make_predict(sample)
+
+   return jsonify({'response':pred})
+   
+   
+   
 
 
 if __name__ == "__main__":
